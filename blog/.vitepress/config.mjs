18ab5426b7a68a8897541bcf578d185a0ed05f4a
鱼梦江湖 <zyyypx@qq.com>
2025-04-01 20:05:22 +0800

    部署

--- blog/.vitepress/config.mjs
+++ blog/.vitepress/config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'vitepress'
 // import { genFeed } from './theme/genFeed'
 import { head } from './theme/head';
-// import { ThemeConfig } from './theme/types';    
 import mdItCustomAttrs from 'markdown-it-custom-attrs'
 import { chineseSearchOptimize, pagefindPlugin } from 'vitepress-plugin-pagefind'
 
@@ -26,8 +25,8 @@ export default defineConfig({
         beecodeurl: 'https://www.appbeebee.com/', //这里是内页ArticleLink组件用到的跳转第三方网址，一般用不到，仅仅用于二次开发
         sidebar: [{}], //这里如果删掉，左侧栏的内容全部不显示。页面布局会变成通栏
         nav: [
-            { text: '好玩的', link: 'https://blog.yumeng.icu' },
-            { text: '好吃的', link: 'https://beebee.work/' }
+            { text: 'Work1', link: 'https://blog.yumeng.icu' },
+            { text: 'Work2', link: 'https://beebee.work/' }
         ],
         music: [ //音乐列表，音乐播放器参数在.vitepress/store/player.ts。封面和歌词不支持可自行按照文章教程修改
             // {
